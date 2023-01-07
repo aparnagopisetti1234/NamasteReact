8032f5fe5e3e428e1c1cae3d36bd0f0f23198b92
Aparna <aparnagopisetti@gmail.com>
2023-01-07 21:46:29 +0530

    Parcel bundler and adding external script file

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**HOT Module Replacement
+ * File Watching Algorithm (written in c++)
+ *Bundling
+ minification
+ cleaning up
+ *Dev and production build
+ superfast build algorithm
+ Image optimization
+ Caching while development
+ Compression
+ Compatible with older version of browser
+ https on dev
+ port Number is managed
+ we have to put parcel-cache in gitignore
+ consistent algorithm
+ Zero Configuration
+ */
+
 const heading1 = React.createElement(
   "h1",
   {
     id: "heading",
+    key: "h1",
   },
   "Hello world"
 );
@@ -10,6 +32,7 @@ const heading2 = React.createElement(
   "h2",
   {
     id: "heading",
+    key: "h2",
   },
   "Hello India"
 );
